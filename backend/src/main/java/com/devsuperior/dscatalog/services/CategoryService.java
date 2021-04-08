5c4abf4f265a2ce8a08a3386a1b3372b0f802be6
lazarofonseca <lazaronobrega3@gmail.com>
2021-04-08 05:52:28 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,8 +2,11 @@ package com.devsuperior.dscatalog.services;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entites.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -14,6 +17,7 @@ public class CategoryService {
 	@Autowired
 	CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return categoryRepository.findAll();
 	}
