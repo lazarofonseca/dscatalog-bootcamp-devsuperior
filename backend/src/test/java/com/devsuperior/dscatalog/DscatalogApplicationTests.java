cf39d2a896d869887e3e7a1d44f011cd46640e81
lazarofonseca <lazaronobrega3@gmail.com>
2021-04-08 05:29:50 -0300

    H2 database

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -1,13 +1,12 @@
-package com.devsuperior.dscatalog;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DscatalogApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.devsuperior.dscatalog;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class DscatalogApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
